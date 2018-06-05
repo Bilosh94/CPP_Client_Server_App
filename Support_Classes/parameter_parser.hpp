053de53bed2aa712caa096b3b5a2072bee92e55e
Unknown <bilos94@gmail.com>
2018-06-05 09:47:43 +0200

    Parameter_parser header
    Deklarace Parameter parseru.

--- Support_Classes/parameter_parser.hpp
+++ Support_Classes/parameter_parser.hpp
@@ -3,3 +3,22 @@
 //	Důvod:	Header třídy třídění parametrů
 
 #pragma once
+
+//Server -m mode -p port
+
+//Klient -h host -p port
+
+class ParameterParser
+{
+private:
+	bool m_mode {false};
+	int m_port {0};
+	string m_host {""};
+
+public:
+	ParameterParser(int t_argc, char* t_argv[]);
+	~ParameterParser();s
+	string getHost();
+	bool getMode();
+	int getPort();
+}
